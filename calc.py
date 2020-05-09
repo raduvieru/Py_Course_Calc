789012dfb26500251d87d22c56067241b842462e
Radu Vieru <radu.vieru@gmail.com>
2020-05-10 00:49:41 +0300

    Petrica first commit

--- calc.py
+++ calc.py
@@ -1,24 +1,35 @@
 def Addition(x, y):
     return x + y
 
-
 def Subtraction(x, y):
     return x - y
 
 def Multiplication(x, y):
-    pass
+    return x * y
 
 def Division(x, y):
-    pass
+    try:
+        div = x / y
+        return div
+    except:
+        return print("Division by zero - error!")
 
 def FloorDivision(x, y):
-    pass
+    try:
+        div = x // y
+        return div
+    except:
+        return print("Division by zero - error!")
 
 def Modulus(x, y):
-    pass
+    try:
+        div = x % y
+        return div
+    except:
+        return print("Division by zero - error!")
 
 def Exponent(x, y):
-    pass
+    return x ** y
 
 
 while True:
@@ -26,7 +37,7 @@ while True:
     operators = ['+', '-', '*', '/', '//', '%', '**']
     if op not in operators:
         break
-    print("Input values for:")
+    print("Input values for:",end='\n')
     a = int(input("A = "))
     b = int(input("B = "))
     if op == '+':
@@ -39,14 +50,14 @@ while True:
         print("A * B =")
         input()
     elif op == '/':
-        print("A / B =")
+        print("A / B =", Division(a,b))
         input()
     elif op == '//':
-        print("A / B =")
+        print("A / B =",FloorDivision(a,b))
         input()
     elif op == '%':
-        print("A % B =")
+        print("A % B =",Modulus(a,b))
         input()
     elif op == '**':
-        print("A ** B =")
+        print("A ** B =",Exponent(a,b))
         input()
