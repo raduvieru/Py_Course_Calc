399f3ded9a4b2cd2f0fd6ac8380a79ac75cef41f
Radu Vieru <radu.vieru@gmail.com>
2020-05-10 01:55:33 +0300

    main name control

--- calc.py
+++ calc.py
@@ -38,39 +38,40 @@ def exponent(x, y):
     return x ** y
 
 
-while True:
-    op = input('Calculator. Press operation key:\n"+" - Addition\n"-" - Subtraction\n"*" - Multiplication\n"/" - '
-               'Division\n"//" - Floor Division\n"%" - Modulus\n"**" - Exponent\n...or any other key for break '
-               'operation\n')
-    operators = ['+', '-', '*', '/', '//', '%', '**']
-    if op not in operators:
-        break
-    print("Input values for:")
-    try:
-        a = int(input("A = "))
-        b = int(input("B = "))
-    except Exception as e:
-        print("Error - ", e)
-        input()
-        continue
-    if op == '+':
-        print("A + B = ", addition(a, b))
-        input()
-    elif op == '-':
-        print("A - B = ", subtraction(a, b))
-        input()
-    elif op == '*':
-        print("A * B =", multiplication(a,b))
-        input()
-    elif op == '/':
-        print("A / B =", division(a, b))
-        input()
-    elif op == '//':
-        print("A // B =", floor_div(a, b))
-        input()
-    elif op == '%':
-        print("A % B =", modulus(a, b))
-        input()
-    elif op == '**':
-        print("A ** B =", exponent(a, b))
-        input()
+if __name__ == "__main__":
+    while True:
+        op = input('Calculator. Press operation key:\n"+" - Addition\n"-" - Subtraction\n"*" - Multiplication\n"/" - '
+                   'Division\n"//" - Floor Division\n"%" - Modulus\n"**" - Exponent\n...or any other key for break '
+                   'operation\n')
+        operators = ['+', '-', '*', '/', '//', '%', '**']
+        if op not in operators:
+            break
+        print("Input values for:")
+        try:
+            a = int(input("A = "))
+            b = int(input("B = "))
+        except Exception as e:
+            print("Error - ", e)
+            input()
+            continue
+        if op == '+':
+            print("A + B = ", addition(a, b))
+            input()
+        elif op == '-':
+            print("A - B = ", subtraction(a, b))
+            input()
+        elif op == '*':
+            print("A * B =", multiplication(a, b))
+            input()
+        elif op == '/':
+            print("A / B =", division(a, b))
+            input()
+        elif op == '//':
+            print("A // B =", floor_div(a, b))
+            input()
+        elif op == '%':
+            print("A % B =", modulus(a, b))
+            input()
+        elif op == '**':
+            print("A ** B =", exponent(a, b))
+            input()
